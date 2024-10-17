0c51bfa3c02c43dc367649e6dd0972cbe3c1337c
Yaghyesh Ghimire <53189528+decipher6@users.noreply.github.com>
2024-10-16 20:29:32 -0400

    Update main.py with fixes for fastapi

--- main.py
+++ main.py
@@ -46,6 +46,16 @@ class Glass(BaseModel):
     Shape: str
     store_id: str
 
+# Helper function to convert ObjectId to string
+def convert_objectid(obj):
+    if isinstance(obj, dict):
+        return {k: convert_objectid(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_objectid(item) for item in obj]
+    elif isinstance(obj, ObjectId):
+        return str(obj)
+    return obj
+
 # Endpoint to add a new store
 @app.post("/add_store")
 async def add_store(store: Store):
@@ -88,9 +98,7 @@ async def get_stores():
     print("Fetching stores...")
     stores = list(stores_collection.find({}))
     print("Stores fetched successfully!")
-    for store in stores:
-        store["_id"] = str(store["_id"])
-    return stores
+    return convert_objectid(stores)
 
 # Endpoint to get all glasses for a specific store
 @app.get("/stores/{store_id}/glasses")
@@ -101,9 +109,7 @@ async def get_glasses_for_store(store_id: str):
         for shape in glasses:
             glass_ids = store["glasses"][shape]
             shape_glasses = list(glasses_collection.find({"_id": {"$in": glass_ids}}))
-            for glass in shape_glasses:
-                glass["_id"] = str(glass["_id"])
-                glasses[shape].append(glass)
+            glasses[shape] = convert_objectid(shape_glasses)
         return glasses
     else:
         raise HTTPException(status_code=404, detail="Store not found")
@@ -115,9 +121,7 @@ async def get_glasses_by_shape(store_id: str, frame_shape: str):
     if store and frame_shape in store["glasses"]:
         glass_ids = store["glasses"][frame_shape]
         shape_glasses = list(glasses_collection.find({"_id": {"$in": glass_ids}}))
-        for glass in shape_glasses:
-            glass["_id"] = str(glass["_id"])
-        return shape_glasses
+        return convert_objectid(shape_glasses)
     else:
         raise HTTPException(status_code=404, detail="No glasses found for this shape in the specified store")
 
@@ -173,163 +177,6 @@ async def search_glasses(
         query["Shape"] = shape
 
     glasses = list(glasses_collection.find(query))
-    for glass in glasses:
-        glass["_id"] = str(glass["_id"])
-
-    return glasses
+    return convert_objectid(glasses)
 
 
-
-
-
-
-
-
-
-# import pymongo
-# from dotenv import load_dotenv
-# import os
-# from fastapi import FastAPI, HTTPException
-# from pydantic import BaseModel
-# from typing import List
-# from bson import ObjectId
-# from fastapi.responses import StreamingResponse
-# from gridfs import GridFS
-# import base64
-
-# # Load environment variables
-# load_dotenv()
-# mongodb_uri = os.getenv('MONGODB_URI')
-
-# # Initialize FastAPI app
-# app = FastAPI()
-
-# # MongoDB client setup
-# try:
-#     client = pymongo.MongoClient(mongodb_uri)
-#     db = client["specommender_db"]
-#     fs = GridFS(db)
-#     print("Successfully connected to the database.")
-# except Exception as e:
-#     print(f"An error occurred while connecting to the database: {e}")
-
-# # Define Pydantic models for request validation
-# class Store(BaseModel):
-#     store_name: str
-#     store_address: str
-#     store_contact: str
-#     store_base_url: str
-
-# class Glass(BaseModel):
-#     frame_shape: str
-#     glass_name: str
-#     price: float
-#     link: str
-#     currency: str
-#     available_colors: List[str]
-#     image_id: str
-
-# # Endpoint to add a new store
-# @app.post("/add_store")
-# async def add_store(store: Store):
-#     store_doc = {
-#         "store_name": store.store_name,
-#         "store_address": store.store_address,
-#         "store_contact": store.store_contact,
-#         "store_base_url": store.store_base_url,
-#         "oval": [],
-#         "round": [],
-#         "square": []
-#     }
-#     db.insert_one(store_doc)
-#     return {"message": "Store added successfully!"}
-
-# # Endpoint to add a new glass to a specific store
-# @app.post("store/add_glass")
-# async def add_glass(glass: Glass):
-#     glass_doc = {
-#         "glass_name": glass.glass_name,
-#         "price": glass.price,
-#         "link": glass.link,
-#         "currency": glass.currency,
-#         "available_colors": glass.available_colors,
-#         "image_id": ObjectId(glass.image_id)
-#     }
-
-#     # Add the glass under the specified frame shape
-#     result = db.update_one(
-#         {"store_name": glass.store_name},
-#         {"$push": {f"{glass.frame_shape}": glass_doc}}
-#     )
-
-#     if result.modified_count == 0:
-#         raise HTTPException(status_code=404, detail="Store not found")
-
-#     return {"message": "Glass added to store successfully!"}
-
-# # Endpoint to get all stores
-# @app.get("/stores")
-# async def get_stores():
-#     stores = list(db.find({}, {"_id": 0}))  # Exclude the MongoDB internal ID
-#     return stores
-
-# # Endpoint to get all glasses for a specific store
-# @app.get("/stores/{store_name}/glasses")
-# async def get_glasses_for_store(store_name: str):
-#     store = stores_collection.find_one({"store_name": store_name}, {"_id": 0, "oval": 1, "round": 1, "square": 1})
-#     if store:
-#         return {
-#             "oval": store.get("oval", []),
-#             "round": store.get("round", []),
-#             "square": store.get("square", [])
-#         }
-#     else:
-#         raise HTTPException(status_code=404, detail="Store not found")
-
-# # Endpoint to get glasses by frame shape for a specific store
-# @app.get("/stores/{store_name}/glasses/{frame_shape}")
-# async def get_glasses_by_shape(store_name: str, frame_shape: str):
-#     store = stores_collection.find_one({"store_name": store_name}, {"_id": 0, f"{frame_shape}": 1})
-#     if store and store.get(frame_shape):
-#         return store[frame_shape]
-#     else:
-#         raise HTTPException(status_code=404, detail="No glasses found for this shape in the specified store")
-
-# # Endpoint to get an image for a specific glass
-# @app.get("/image/{glass_id}")
-# async def get_image(glass_id: str):
-#     # Retrieve the glass document from the database
-#     glass = stores_collection.find_one({"$or": [
-#         {"oval.image_id": ObjectId(glass_id)},
-#         {"round.image_id": ObjectId(glass_id)},
-#         {"square.image_id": ObjectId(glass_id)}
-#     ]})
-
-#     # If the glass doesn't exist, return a 404 error
-#     if not glass:
-#         raise HTTPException(status_code=404, detail="Glass not found")
-
-#     # Get the ObjectId of the image for the glass
-#     image_id = None
-#     for shape in ["oval", "round", "square"]:
-#         for g in glass.get(shape, []):
-#             if str(g["image_id"]) == glass_id:
-#                 image_id = g["image_id"]
-#                 break
-#         if image_id:
-#             break
-
-#     # Retrieve the image from GridFS
-#     image = fs.get(image_id)
-
-#     # Encode the image data as base64
-#     image_data = base64.b64encode(image.read()).decode('utf-8')
-
-#     # Return the glass information as a JSON response, including the image data
-#     return {
-#         "glass_name": g["glass_name"],
-#         "price": g["price"],
-#         "available_colors": g["available_colors"],
-#         "link": g["link"],
-#         "image_data": image_data
-#     }
